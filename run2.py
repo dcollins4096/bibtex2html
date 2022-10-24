989b2fcc17f01bd036edf8e66a22ee37dbd108c0
dcollins <dccollins@fsu.edu>
2022-10-24 12:39:33 -0400

    misc kruft

--- run2.py
+++ run2.py
@@ -8,7 +8,7 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-b", "--bibtex", dest="bibtex",action="store",
                                     help = "bibtex file",
-                                    default="export-bibtex.bib", type = str)
+                                    default="main.bib", type = str)
 parser.add_option("-o", "--output", dest="output",action="store",
                                     help = "output file",
                                     default="output.html", type = str)
@@ -20,13 +20,16 @@ parser.add_option("-t", "--template", dest="template",action="store",
 records = b2h.parse_bibtex( options.bibtex)
 
 categories = b2h.get_categories( records)
-print( categories.keys())
 
 loader=jinja2.FileSystemLoader('.')
 env = jinja2.Environment(loader=loader)
 template = env.get_template(options.template)
 
-category_list = list(categories.keys())
+all_categories = list(categories.keys())
+category_list = ['Star Formation','Supernova','Turbulence','Galaxies','CMB Foregrounds']
+#make sure you got everything
+aset = set(all_categories); bset=set(category_list)
+category_list += list( aset-bset)
 optr=open(options.output,'w')
 optr.write(template.render(categories=categories, category_list=category_list))
 optr.close()
